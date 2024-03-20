d87ed169fb905dbcb83f3b919ca8b306e81e5145
Sammmte <santiagoperez.-@hotmail.com>
2024-03-20 14:29:01 -0300

    substracted complex element example from test button script

--- Assets/TestProject/TestButton.cs
+++ Assets/TestProject/TestButton.cs
@@ -11,22 +11,3 @@ public class TestButton : Button
         text = "Test Button";
     }
 }
-
-[MainToolbarElement(group: "Left Test Group")]
-public class ComplexElement : VisualElement
-{
-    public ComplexElement()
-    {
-        Add(new Label("This is a complex element"));
-
-        var button1 = new Button(() => Debug.Log("ComplexElement button pressed"));
-        button1.text = "Button 1";
-
-        var button2 = new Button(() => Debug.Log("another ComplexElement button pressed"));
-        button2.text = "Button 2";
-
-        Add(button1);
-        Add(button2);
-        Add(new Toggle("Just a toggle"));
-    }
-}
