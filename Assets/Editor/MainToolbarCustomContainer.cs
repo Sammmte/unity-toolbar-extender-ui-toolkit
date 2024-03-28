fb08e15d00236fa82952aca1edff296cf3a16881
Sammmte <santiagoperez.-@hotmail.com>
2024-03-28 02:56:30 -0300

    minor change on scroll view height

--- Assets/Editor/MainToolbarCustomContainer.cs
+++ Assets/Editor/MainToolbarCustomContainer.cs
@@ -5,11 +5,12 @@ namespace Paps.UnityToolbarExtenderUIToolkit
 {
     public class MainToolbarCustomContainer : VisualElement
     {
-        private const int SCROLL_VIEW_SCROLLER_HEIGHT = 4;
-        private const int SCROLLER_LOW_BUTTON_HEIGHT = 7;
-        private const int SCROLLER_HIGH_BUTTON_HEIGHT = 7;
-        private const int SCROLLER_SLIDER_HEIGHT = 7;
-        private const int SCROLL_VIEW_HORIZONTAL_PADDING = 4;
+        private const int SCROLL_VIEW_SCROLLER_HEIGHT = 2;
+        private const int SCROLLER_LOW_BUTTON_HEIGHT = 6;
+        private const int SCROLLER_HIGH_BUTTON_HEIGHT = 6;
+        private const int SCROLLER_SLIDER_HEIGHT = 6;
+        private const int SCROLL_VIEW_HORIZONTAL_PADDING = 0;
+        private const int SCROLL_VIEW_SCROLLER_BORDER_TOP_WIDTH = 0;
 
         private ScrollView _scrollView;
 
@@ -41,6 +42,7 @@ namespace Paps.UnityToolbarExtenderUIToolkit
             var slider = scroller.slider;
 
             scroller.style.height = SCROLL_VIEW_SCROLLER_HEIGHT;
+            scroller.style.borderTopWidth = SCROLL_VIEW_SCROLLER_BORDER_TOP_WIDTH;
             leftButton.style.height = SCROLLER_LOW_BUTTON_HEIGHT;
             rightButton.style.height = SCROLLER_HIGH_BUTTON_HEIGHT;
             slider.style.height = SCROLLER_SLIDER_HEIGHT;
