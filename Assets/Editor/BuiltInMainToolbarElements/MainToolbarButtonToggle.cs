63bdbb8d0287639713d80b134ea140c076ebc7a6
Sammmte <santiagoperez.-@hotmail.com>
2024-03-31 01:34:07 -0300

    added main toolbar button toggle as built in element

---
+++ Assets/Editor/BuiltInMainToolbarElements/MainToolbarButtonToggle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor.Toolbars;
+using UnityEngine;
+
+namespace Paps.UnityToolbarExtenderUIToolkit
+{
+    public class MainToolbarButtonToggle : EditorToolbarToggle
+    {
+        public MainToolbarButtonToggle() : base()
+        {
+
+        }
+
+        public MainToolbarButtonToggle(string text) : base(text)
+        {
+
+        }
+
+        public MainToolbarButtonToggle(Texture2D icon) : base(icon)
+        {
+
+        }
+
+        public MainToolbarButtonToggle(Texture2D iconOn, Texture2D iconOff) : base(iconOn, iconOff)
+        {
+
+        }
+
+        public MainToolbarButtonToggle(string text, Texture2D iconOn, Texture2D iconOff) : base(text, iconOn, iconOff)
+        {
+
+        }
+    }
+}
---
+++ Assets/TestProject/TestToggleButtons.cs
@@ -0,0 +1,10 @@
+using Paps.UnityToolbarExtenderUIToolkit;
+
+[MainToolbarElement(order: -4)]
+public class MyButtonToggle : MainToolbarButtonToggle
+{
+    public MyButtonToggle() : base("HOLO")
+    {
+
+    }
+}
