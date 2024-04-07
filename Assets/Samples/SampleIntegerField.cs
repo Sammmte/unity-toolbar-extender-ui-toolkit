89109b34cae94dfa4cfeac58f0822f988c90cfbf
Sammmte <santiagoperez.-@hotmail.com>
2024-04-07 10:00:15 -0300

    minor change on some samples

---
+++ Assets/Samples/SampleIntegerField.cs
@@ -0,0 +1,13 @@
+﻿using Paps.UnityToolbarExtenderUIToolkit;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[MainToolbarElement(alignWhenSingle: ToolbarAlign.Left, order: 3)]
+public class SampleIntegerField : IntegerField
+{
+    public SampleIntegerField()
+    {
+        label = "Sample Integer Field";
+        RegisterCallback<ChangeEvent<int>>(eventArgs => Debug.Log("Sample Integer Field changed value to " + eventArgs.newValue));
+    }
+}
--- Assets/Samples/SampleTextField.cs
+++ Assets/Samples/SampleTextField.cs
@@ -7,19 +7,7 @@ public class SampleTextField : TextField
 {
     public SampleTextField()
     {
-        style.flexDirection = FlexDirection.Column;
         label = "Sample Text Field";
         RegisterCallback<ChangeEvent<string>>(eventArgs => Debug.Log("Sample Text Field changed value to " + eventArgs.newValue));
-    }
-}
-
-[MainToolbarElement(alignWhenSingle: ToolbarAlign.Left, order: 3)]
-public class SampleIntegerField : IntegerField
-{
-    public SampleIntegerField()
-    {
-        style.flexDirection = FlexDirection.Column;
-        label = "Sample Integer Field";
-        RegisterCallback<ChangeEvent<int>>(eventArgs => Debug.Log("Sample Integer Field changed value to " + eventArgs.newValue));
     }
 }
