490085c41c17260ff7a2da3a1b9146cf42d71ea1
Sammmte <santiagoperez.-@hotmail.com>
2024-04-08 21:51:35 -0300

    using confirmation popups to ask user before deleting editor prefs

--- Assets/Package/Editor/Helpers/MenuItems.cs
+++ Assets/Package/Editor/Helpers/MenuItems.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 
 namespace Paps.UnityToolbarExtenderUIToolkit
 {
@@ -19,14 +20,39 @@ namespace Paps.UnityToolbarExtenderUIToolkit
         [MenuItem(ToolInfo.EDITOR_MENU_BASE + "/Reset Overrides")]
         public static void ResetOverrides()
         {
-            ServicesAndRepositories.MainToolbarElementOverridesRepository.Clear();
-            MainToolbarAutomaticExtender.Refresh();
+            ShowDialog(
+                "Reset Overrides",
+                "You are about to reset all toolbar elements overrides.\nAre you sure you want to continue?",
+                "Reset",
+                "Cancel",
+                () =>
+                {
+                    ServicesAndRepositories.MainToolbarElementOverridesRepository.Clear();
+                    MainToolbarAutomaticExtender.Refresh();
+                }
+                );
         }
 
         [MenuItem(ToolInfo.EDITOR_MENU_BASE + "/Delete Package Related EditorPrefs")]
         public static void DeletePackageRelatedEditorPrefs()
         {
-            JsonEditorPrefs.DeleteAll();
+            ShowDialog(
+                "Delete Package Related Editor Prefs",
+                $"You are about to delete all Editor Prefs related to {ToolInfo.FRIENDLY_TOOL_NAME}.\nAre you sure you want to continue?",
+                "Delete",
+                "Cancel",
+                () => JsonEditorPrefs.DeleteAll()
+                );
         }
+
+        private static void ShowDialog(string title, string message, string okMessage, 
+            string cancelMessage, Action onOk = null, Action onCancel = null)
+        {
+            if(EditorUtility.DisplayDialog(title, message, okMessage, cancelMessage))
+                onOk?.Invoke();
+            else
+                onCancel?.Invoke();
+        }
+
     }
 }
