09a465af5c25eb9e0aae041cc864dc39550c36f9
Sammmte <santiagoperez.-@hotmail.com>
2024-04-11 10:59:19 -0300

    removed unused usings

--- Assets/Package/Editor/GroupDefinitions/MainToolbarElementTypeDropdownDrawer.cs
+++ Assets/Package/Editor/GroupDefinitions/MainToolbarElementTypeDropdownDrawer.cs
@@ -1,9 +1,8 @@
-using System.Collections;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine.UIElements;
-using System.Collections.Generic;
-using System;
 
 namespace Paps.UnityToolbarExtenderUIToolkit
 {
