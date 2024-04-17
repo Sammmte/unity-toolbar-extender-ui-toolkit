784929ef385c0e17637cbd186eb561c15076c4ba
Sammmte <santiagoperez.-@hotmail.com>
2024-04-17 19:42:07 -0300

    group popup won't close if focused window is a popup window with popup content marked with GroupPopupSubWindowAttribute

--- Assets/Package/Editor/GroupDefinitions/GroupDropdownWindowPopupManager.cs
+++ Assets/Package/Editor/GroupDefinitions/GroupDropdownWindowPopupManager.cs
@@ -1,19 +1,27 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using PopupWindow = UnityEditor.PopupWindow;
 
 namespace Paps.UnityToolbarExtenderUIToolkit
 {
     internal static class GroupDropdownWindowPopupManager
     {
+        private const string POPUP_WINDOW_CONTENT_FIELD_NAME = "m_WindowContent";
+
+        private static FieldInfo _popupWindowContentField;
         private static Type[] _subWindowTypes = new Type[0];
         private static List<GroupDropdownWindowPopup> _windows = new List<GroupDropdownWindowPopup>();
 
         static GroupDropdownWindowPopupManager()
         {
+            _popupWindowContentField = typeof(PopupWindow)
+                    .GetField(POPUP_WINDOW_CONTENT_FIELD_NAME,
+                    BindingFlags.NonPublic | BindingFlags.Instance);
             LoadSubWindowTypes();
             EditorApplication.update += Update;
         }
@@ -21,8 +29,28 @@ namespace Paps.UnityToolbarExtenderUIToolkit
         private static void LoadSubWindowTypes()
         {
             _subWindowTypes = TypeCache.GetTypesWithAttribute<GroupPopupSubWindowAttribute>()
-                            .Where(type => typeof(EditorWindow).IsAssignableFrom(type))
+                            .Where(type => IsValidSubWindowType(type))
                             .ToArray();
+        }
+
+        private static bool IsValidSubWindowType(Type type)
+        {
+            return typeof(EditorWindow).IsAssignableFrom(type) ||
+                typeof(PopupWindowContent).IsAssignableFrom(type);
+        }
+
+        private static bool ContainsValidPopupWindowContent(EditorWindow window)
+        {
+            if(window is PopupWindow popupWindow)
+            {
+                var popupContent = _popupWindowContentField.GetValue(popupWindow) as PopupWindowContent;
+                
+                var popupContentSpecificType = popupContent.GetType();
+
+                return _subWindowTypes.Contains(popupContentSpecificType);
+            }
+
+            return false;
         }
 
         private static void Update()
@@ -61,7 +89,8 @@ namespace Paps.UnityToolbarExtenderUIToolkit
         private static bool FocusedWindowIsValid()
         {
             return _windows.Contains(EditorWindow.focusedWindow) || 
-                _subWindowTypes.Contains(EditorWindow.focusedWindow.GetType());
+                _subWindowTypes.Contains(EditorWindow.focusedWindow.GetType()) ||
+                ContainsValidPopupWindowContent(EditorWindow.focusedWindow);
         }
 
         public static void Show(Rect activatorRect, VisualElement[] elements)
