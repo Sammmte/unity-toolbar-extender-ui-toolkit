11527326a7d8523856fe58023bf02bc31042f293
Sammmte <santiagoperez.-@hotmail.com>
2024-04-18 06:15:20 -0300

    set flex wrap to wrap again on group childs

--- Assets/Package/Editor/GroupDefinitions/GroupDropdownWindowPopup.cs
+++ Assets/Package/Editor/GroupDefinitions/GroupDropdownWindowPopup.cs
@@ -36,6 +36,7 @@ namespace Paps.UnityToolbarExtenderUIToolkit
             {
                 var container = CreateGroupElementContainer();
                 container.Add(groupElement);
+                groupElement.style.flexWrap = Wrap.Wrap;
                 container.style.display = groupElement.style.display;
                 _scrollView.Add(container);
             }
