d8db92facd0634617838f22d3c7831eaef7b0f94
Sammmte <santiagoperez.-@hotmail.com>
2024-04-19 16:31:01 -0300

    removed unused usings

--- Assets/Package/Editor/Helpers/GlobalActions.cs
+++ Assets/Package/Editor/Helpers/GlobalActions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UnityEditor;
-using UnityEngine;
 
 namespace Paps.UnityToolbarExtenderUIToolkit
 {
--- Assets/Package/Editor/MainToolbarAutomaticExtender.cs
+++ Assets/Package/Editor/MainToolbarAutomaticExtender.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Paps.UnityToolbarExtenderUIToolkit
