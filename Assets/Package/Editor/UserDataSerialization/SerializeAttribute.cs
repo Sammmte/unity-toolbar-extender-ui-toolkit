a88a44ad24d0172029ce983721ee848f026a3f53
Sammmte <santiagoperez.-@hotmail.com>
2024-07-20 23:05:22 -0300

    simplified serialize attribute, for now

--- Assets/Package/Editor/UserDataSerialization/SerializeAttribute.cs
+++ Assets/Package/Editor/UserDataSerialization/SerializeAttribute.cs
@@ -5,16 +5,6 @@ namespace Paps.UnityToolbarExtenderUIToolkit
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
     public class SerializeAttribute : Attribute
     {
-        public string Id { get; private set; }
 
-        public SerializeAttribute(string id = null)
-        {
-            SetId(id);
-        }
-
-        internal void SetId(string id)
-        {
-            Id = id;
-        }
     }
 }
