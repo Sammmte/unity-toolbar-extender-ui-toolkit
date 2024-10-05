60110e780422e202193d46440eb80e6aa69f9ff5
Sammmte <santiagoperez.-@hotmail.com>
2024-10-05 03:34:20 -0300

    added samples for vector 2 and vector 3 fields

---
+++ Assets/Samples/BasicElements/SampleVector2Field.cs
@@ -0,0 +1,11 @@
+﻿using Paps.UnityToolbarExtenderUIToolkit;
+using UnityEngine.UIElements;
+
+[MainToolbarElement("SampleVector2Field")]
+public class SampleVector2Field : Vector2Field
+{
+    public SampleVector2Field() : base("Sample Vector 2")
+    {
+
+    }
+}
---
+++ Assets/Samples/BasicElements/SampleVector3Field.cs
@@ -0,0 +1,11 @@
+﻿using Paps.UnityToolbarExtenderUIToolkit;
+using UnityEngine.UIElements;
+
+[MainToolbarElement("SampleVector3Field")]
+public class SampleVector3Field : Vector3Field
+{
+    public SampleVector3Field() : base("Sample Vector 3")
+    {
+
+    }
+}
